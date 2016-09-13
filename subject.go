3697dd97f56da7e868cbecc11917cd69bcae4157
Kasey Klipsch <kklipsch@mediamath.com>
2016-09-13 08:27:25 -0500

    add copyright header

--- subject.go
+++ subject.go
@@ -1,5 +1,9 @@
 package sr
 
+//Copyright 2016 MediaMath <http://www.mediamath.com>.  All rights reserved.
+//Use of this source code is governed by a BSD-style
+//license that can be found in the LICENSE file.
+
 import "fmt"
 
 //Subject is *not* a topic.  Subject is a schema registry abstraction, topic is a kafka one.  For instance the kafka rest proxy assumes that for
